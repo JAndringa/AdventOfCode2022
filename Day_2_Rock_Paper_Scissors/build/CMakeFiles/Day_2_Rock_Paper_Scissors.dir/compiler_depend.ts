9d1c0290aaa8e8a301124ac47cd16f220980a1e8
JAndringa <andringajibbe@gmail.com>
2022-12-05 16:43:47 +0100

    git ignore updated

--- Day_2_Rock_Paper_Scissors/build/CMakeFiles/Day_2_Rock_Paper_Scissors.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Day_2_Rock_Paper_Scissors.
--- Day_2_Rock_Paper_Scissors/build/CMakeFiles/Day_2_Rock_Paper_Scissors.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Day_2_Rock_Paper_Scissors.
-# This may be replaced when dependencies are built.
